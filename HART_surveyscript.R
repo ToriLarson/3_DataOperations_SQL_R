a16d392281874479f7a2714be4baf18f6ca47187
Tori Larson <ToriLarson@users.noreply.github.com>
2017-12-19 20:33:55 -1000

    added saved R files

--- HART_surveyscript.R
+++ HART_surveyscript.R
@@ -1,5 +1,70 @@
 ## this is my first r script - I am learning how to use scripts to make my life easier
 
-# this is what I named my file 
+# set path 
+setwd("/Users/victorialarson/Desktop/Dev_League/Repos/Sprint3")
+
+# open file 
+read.csv("HART_survey_data.csv")
+
+# name file
 HART_survey=read.csv("HART_survey_data.csv")
 
+#make a histogram
+hist(HART_survey$ADULTS.IN.HH_Code)
+
+# FUN FACT: c makes a vector
+HART_survey:c[]
+
+
+FemaleTotal <- sum(HART_survey$GENDER_Text == 'Female')
+ft3print(FemaleTotal)
+
+ft3 <- HART_survey$GENDER_Text == 'Female'
+
+MaleTotal <- sum(HART_survey$GENDER_Text == 'Male')
+
+print(MaleTotal)
+
+## column
+ft4 <- HART_survey[ft3,]
+
+#df[gendertext]
+#df[rowvector,]
+#df[rowvectpor, colvector]
+
+##print column names
+colnames(ft4)
+
+
+"alligator" > "crocodile"
+"crocodile" > "alligator"
+"tori" = 3
+"tori" == "tori"
+"tori" == 1+2
+"tori" != "ben"
+c("victoria", "dawn", "ben", "loverbear", "trevino", "larson") == "trevino"
+
+length(HART_survey$WILL.GO.TO.SCHOOL.BEFORE.GOING.HOME_Text)
+HART_survey[c("GENDER_Text","GENDER_Code")]
+HART_survey[c("GENDER_Text","GENDER_Code")]
+class(HART_survey[c("GENDER_Text","GENDER_Code")])
+
+## subsets are cool - use it as a filter.
+subset(data.framename, select = columnname)
+##what is select doing
+##why dont you have to put it in " "
+
+## Saving entire workspace 
+save.image(file = "HART_survey.RData")
+
+# Saving on object in RData format
+save(data1, file = "data.RData")
+
+# Save multiple objects
+save(data1, data2, file = "data.RData")
+
+#load the file that you saved
+load("HART_survey.RData")
+
+save("ft4", file = "ft4.RData")
+load("ft4.RDATA")
